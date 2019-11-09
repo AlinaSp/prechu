114de60b2edceed907b271b15072e1a3180fdbe6
AlinaSp <alinasapiannik@gmail.com>
2019-11-09 15:10:10 +0200

    added slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var sliderHero = new Swiper ('.slider-hero', {
+    loop: true,
+    autoplay: {
+    delay: 3000,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
